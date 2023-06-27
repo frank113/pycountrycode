534eae9cadd204a65e989ca4306e93b2eda78359
Francis R. Kovacs <frankkovacs8@gmail.com>
2023-06-27 01:11:12 -0400

    update formatting in countrycode

--- countrycode/__init__.py
+++ countrycode/__init__.py
@@ -1,1 +1,8 @@
+"""
+`countrycode` exports.
+
+Values:
+`codelist`: A dataframe of `countrycode/data/codelist.csv` read via polars
+`countrycode`: Function to convert country codes or names from one format to another.
+"""
 from .countrycode import codelist, countrycode
--- countrycode/countrycode.py
+++ countrycode/countrycode.py
@@ -1,23 +1,27 @@
+"""
+Country code function and code list definition
+"""
 import re
 import os
 import polars as pl
-from copy import copy
 
 pkg_dir, pkg_filename = os.path.split(__file__)
 data_path = os.path.join(pkg_dir, "data", "codelist.csv")
 codelist = pl.read_csv(data_path)
 
 
-def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.name.en'):
+def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.name.en'): # pylint: disable=dangerous-default-value,too-many-branches
     """
     Convert country codes or names from one format to another.
 
-    This function takes a list, string, or Polars Series of country codes or names, and converts them to the desired
-    format, such as ISO 3-letter codes, country names in different languages, etc.
+    This function takes a list, string, or Polars Series of country codes or names,
+    and converts them to the desired format, such as ISO 3-letter codes, country
+    names in different languages, etc.
 
     Parameters:
     sourcevar (list, str, or polars.series.series.Series, optional):
-        A list, string, or Polars Series of country codes or names to be converted. Default is ['DZA', 'CAN'].
+        A list, string, or Polars Series of country codes or names to be converted.
+        Default is ['DZA', 'CAN'].
     origin (str, optional):
         The format of the input country codes or names. Default is 'iso3c'.
     destination (str, optional):
@@ -25,7 +29,8 @@ def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.n
 
     Returns:
     list, str, or polars.series.series.Series:
-        The converted country codes or names in the desired format. The output type depends on the input type:
+        The converted country codes or names in the desired format. The output type depends
+        on the input type:
         - If `sourcevar` is a string or int, returns a string.
         - If `sourcevar` is a list, returns a list.
         - If `sourcevar` is a Polars Series, returns a Polars Series.
@@ -40,7 +45,8 @@ def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.n
     ['Algeria', 'Canada']
 
     Note:
-    This function uses two helper functions (`replace_regex` and `replace_exact`) to perform the actual conversion.
+    This function uses two helper functions (`replace_regex` and `replace_exact`) to perform the
+    actual conversion.
     """
 
     # user convenience shortcuts
@@ -54,9 +60,14 @@ def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.n
         destination = "country.name.en"
 
     if destination not in codelist.columns:
-        raise ValueError(f"destination must be one of: " + ".join(codelist.columns)")
+        raise ValueError("destination must be one of: " + "".join(codelist.columns))
 
-    valid = ["cctld", "country.name", "country.name.de", "country.name.fr", "country.name.it", "cowc", "cown", "dhs", "ecb", "eurostat", "fao", "fips", "gaul", "genc2c", "genc3c", "genc3n", "gwc", "gwn", "imf", "ioc", "iso2c", "iso3c", "iso3n", "p5c", "p5n", "p4c", "p4n", "un", "un_m49", "unicode.symbol", "unhcr", "unpd", "vdem", "wb", "wb_api2c", "wb_api3c", "wvs", "country.name.en.regex", "country.name.de.regex", "country.name.fr.regex", "country.name.it.regex"]
+    valid = ["cctld", "country.name", "country.name.de", "country.name.fr", "country.name.it",
+             "cowc", "cown", "dhs", "ecb", "eurostat", "fao", "fips", "gaul", "genc2c", "genc3c",
+             "genc3n", "gwc", "gwn", "imf", "ioc", "iso2c", "iso3c", "iso3n", "p5c", "p5n", "p4c",
+             "p4n", "un", "un_m49", "unicode.symbol", "unhcr", "unpd", "vdem", "wb", "wb_api2c",
+             "wb_api3c", "wvs", "country.name.en.regex", "country.name.de.regex",
+             "country.name.fr.regex", "country.name.it.regex"]
     if origin not in valid:
         raise ValueError("origin must be one of: " + ", ".join(valid))
 
@@ -68,10 +79,11 @@ def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.n
     elif isinstance(sourcevar, pl.series.series.Series):
         sourcevar_series = sourcevar
     else:
-        raise ValueError(f"sourcevar must be a string, list, or polars series. Got {type(sourcevar)}")
+        raise ValueError(f"sourcevar must be a string, list, or polars series. Got {type(sourcevar)}") # pylint: disable=line-too-long
 
     # conversion
-    if origin in ['country.name.en.regex', 'country.name.fr.regex', 'country.name.de.regex', 'country.name.it.regex']:
+    if origin in ['country.name.en.regex', 'country.name.fr.regex', 'country.name.de.regex',
+                  'country.name.it.regex']:
         out = replace_regex(sourcevar_series, origin, destination)
     else:
         out = replace_exact(sourcevar_series, origin, destination)
@@ -79,15 +91,23 @@ def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.n
     # output type
     if isinstance(sourcevar, str) | isinstance(sourcevar, int):
         return out[0]
-    elif isinstance(sourcevar, list) & isinstance(out, pl.series.series.Series):
+    if isinstance(sourcevar, list) & isinstance(out, pl.series.series.Series):
         return out.to_list()
-    elif isinstance(out, list) & isinstance(sourcevar, pl.series.series.Series):
+    if isinstance(out, list) & isinstance(sourcevar, pl.series.series.Series):
         return pl.Series(out)
-    else:
-        return out
+    return out
 
 
 def get_first_match(pattern, string_list):
+    """
+    Determine the first match of pattern in string_list.
+    Args:
+        pattern:
+        string_list:
+
+    Returns:
+
+    """
     for string in string_list:
         match = pattern.search(string)
         if match:
@@ -96,6 +116,16 @@ def get_first_match(pattern, string_list):
 
 
 def replace_exact(sourcevar, origin, destination):
+    """
+    Replace exact
+    Args:
+        sourcevar:
+        origin:
+        destination:
+
+    Returns:
+
+    """
     codelist_nonull = codelist[[origin, destination]].drop_nulls()
     mapping = dict(zip(codelist_nonull[origin], codelist_nonull[destination]))
     out = sourcevar.map_dict(mapping)
@@ -103,10 +133,21 @@ def replace_exact(sourcevar, origin, destination):
 
 
 def replace_regex(sourcevar, origin, destination):
+    """
+    Helper function to replace a regular expression
+    Args:
+        sourcevar:
+        origin:
+        destination:
+
+    Returns:
+
+    Todo: Handle o and d variable names
+    """
     sourcevar_unique = sourcevar.unique()
     codelist_nonull = codelist[[origin, destination]].drop_nulls()
-    o = [re.compile(x, flags = re.IGNORECASE) for x in codelist_nonull[origin]]
-    d = codelist_nonull[destination]
+    o = [re.compile(x, flags = re.IGNORECASE) for x in codelist_nonull[origin]] # pylint: disable=invalid-name
+    d = codelist_nonull[destination] # pylint: disable=invalid-name
     result = []
     for string in sourcevar_unique:
         match_found = False
